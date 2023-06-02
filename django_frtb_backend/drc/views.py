c6d858cc84617cda55887ca25721ad6cc99ce052
Sigirisetty Surya Kiran <sigirisetti@gmail.com>
2023-06-02 18:43:21 +0800

    calib summary controller update

--- django_frtb_backend/drc/views.py
+++ django_frtb_backend/drc/views.py
@@ -1,18 +1,13 @@
-from django.shortcuts import render, HttpResponseRedirect
-from django.http import HttpResponse, JsonResponse
-
+from django.http import JsonResponse
 from drc.models import DrcCalibrationSummary
 from drc.serializers import DrcModelSerializer
 
-from django.views.decorators.csrf import csrf_exempt
 
-
-@csrf_exempt
 def get_drc_calib_summary(request):
     print("Executing get_drc_calib_summary")
     data = DrcCalibrationSummary.objects.all()
     if request.method == 'GET':
         serializer = DrcModelSerializer(data, many=True)
         r = JsonResponse(serializer.data, safe=False)
-        print(r)
+        # print(serializers.serialize('json', data))
         return r
